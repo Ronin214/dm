28162db968553f96cc6a734de29193e791920737
Ronin214 <ronpon214@gmail.com>
2025-03-19 21:36:57 +0400

    Add files via upload

---
+++ dm/main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+
+window = Tk()
+window.title("DM")
+window.geometry("300x300")
+
+messagebox.showinfo("DW Ver", "v0.1")
+
+urlInpit = Frame(borderwidth=)
+
+window.mainloop()
