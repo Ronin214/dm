7c2a23563627d948f38d3a0dbabd971d380ca9af
Ronin214 <ronpon214@gmail.com>
2025-03-26 10:24:18 +0400

    Update dm.py

--- dm.py
+++ dm.py
@@ -2,16 +2,25 @@ from sys import argv
 
 import os
 
+
 if argv[1] == "-h" or argv[1] == "-help" :
-    print("dm url = dowload file")
-    print("\n -h / -help = viwe commands")
-    print(" -v / -version = viwe version")
-    print(" -odm / -oldm = use old dm version")
+    print("dm {url} = dowload audio file")
+
+    print("\n-dv || -dowloadVideo = dowload video")
+    print("exemple = dm {url} -dv = dowload video file")
+
+    print("\n----------------------------------------------")
+
+    print("\n -h || -help = viwe commands")
+    print(" -v || -version = viwe version")
+    print(" -odm || -oldm = use old dm version")
+
     print("\n made by zag ^_^")
 
+
 elif argv[1] == "-v" or argv[1] == "-version" :
-    print("dm version - 0.2")
-    print("made in 25.03.2025")
+    print("dm version - 0.2.1")
+    print("made in 26.03.2025")
 
 elif argv[1] == "-odm" or argv[1] == "-oldm" :
     
@@ -25,8 +34,15 @@ elif argv[1] == "-odm" or argv[1] == "-oldm" :
 
         userCountMusic = userCountMusic - 1
 
+
 else :
 
     url = argv[1]
 
-    os.system(f"yt-dlp -x --audio-format mp3 --embed-thumbnail --embed-metadata  {url}")
+    if argv[2] == "-dv" or argv[2] == "-dowloadVideo" :
+        ytDlpPrefix = "--embed-thumbnail --embed-metadata"
+    
+    else :
+        ytDlpPrefix = "-x --audio-format mp3 --embed-thumbnail --embed-metadata"
+
+    os.system(f"yt-dlp {ytDlpPrefix} {url}")
